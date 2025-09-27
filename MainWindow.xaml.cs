a8ac4190c7c447c23dae4dd1dc9e9278e63d0bf3
Gregory Bowne <gbowne144@gmail.com>
2025-09-27 16:45:28 -0700

    Create MainWindow.xaml.cs
    adding initial window stuff

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace WeatherDashboard.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
