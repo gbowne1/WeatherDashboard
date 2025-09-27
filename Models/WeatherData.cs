bc4fdd30c3e153f1d350996e978bb27db5e73bc3
Gregory Bowne <gbowne144@gmail.com>
2025-09-27 16:38:53 -0700

    Update WeatherData.cs
    add a Model

--- Models/WeatherData.cs
+++ Models/WeatherData.cs
@@ -1,2 +1,16 @@
-// Placeholder
 using System;
+
+namespace WeatherDashboard.Models
+{
+    public class WeatherData
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+        public double Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string ApiSource { get; set; }
+    }
+}
